f2eb9862f8e50e819de308dc4cb09d1f9525e465
evaldo <evaldo.junior@etec.sp.gov.br>
2021-11-05 20:36:34 -0300

    Formulário de Contatos

--- lib/contato_screen.dart
+++ lib/contato_screen.dart
@@ -7,8 +7,47 @@ class ContatoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Novo Contato"),
+        title: const Text("Novo Contato"),
         centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    height: 140,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.scaleDown,
+                            image: ExactAssetImage('images/person.png')),
+                        boxShadow: kElevationToShadow[1]),
+                  )),
+                ],
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(label: Text("Nome")),
+            ),
+            const TextField(
+              decoration: InputDecoration(label: Text("Email")),
+            ),
+            const TextField(
+              decoration: InputDecoration(label: Text("Telefone")),
+            ),
+          ],
+        ),
       ),
     );
   }
